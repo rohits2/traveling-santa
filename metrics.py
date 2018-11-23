175db1d4af1ccbf637e723ea78bd61d578f4038e
Rohit Singh <singhrohit2@hotmail.com>
2018-11-22 21:52:32 -0700

    Fix metric to accurately reflect competition

--- metrics.py
+++ metrics.py
@@ -6,8 +6,7 @@ import numpy.linalg as la
 def metric(path):
     visited_cities = cities[path, :]
     distances = la.norm(visited_cities[1:] - visited_cities[:-1], axis=1)
-    for i in range(0, len(path), 10):
-        print(i)
-        if path[i] not in prime_set and i != 0:
+    for i in range(9, len(path)-1, 10):
+        if path[i] not in prime_set:
             distances[i] *= 1.1
     return distances.sum()
